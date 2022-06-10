cafd0b3c8b2301420375e2a0af7b44f3b8d5ed5e
Lidia <lidia.baciu97@gmail.com>
2022-06-10 19:04:45 +0200

    add missing default property

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,11 +1,13 @@
 variable "resource_group_name" {
   description = "Name of the resource group containg the state"
   type        = string
+  default     = "rootsac2022q1edge"
 }
 
 variable "storage_account_name" {
   description = "Name of the storage account containg the state"
   type        = string
+  default     = "rootsac2022storage"
 }
 
 variable "container_name" {
