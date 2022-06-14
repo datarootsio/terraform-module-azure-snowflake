b0a32305aa84c62e4f0cea197fd9ecea09b14f5e
Lidia <lidia.baciu97@gmail.com>
2022-06-14 17:29:59 +0200

    update resource group name for backend

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rootsac2022q1edge"
+    resource_group_name  = "rg-terraformingsnowflake"
     storage_account_name = "rootsac2022storage"
     container_name       = "tfstate"
   }
