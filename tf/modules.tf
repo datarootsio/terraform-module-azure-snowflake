2a1075de69cb3005f1f6eb25abfd0aa2b0210bd2
Lidia <lidia.baciu97@gmail.com>
2022-06-14 18:48:59 +0200

    add alias and role to snowflake module

--- tf/modules.tf
+++ tf/modules.tf
@@ -7,6 +7,9 @@ module "snowflake" {
   source = "./snowflake"
   env    = var.env
 
+  snowflake_alias = var.snowflake_alias
+  snowflake_role  = var.snowflake_role
+
   sas_token             = module.azure.snowflake_sas_token
   sas_token_predictions = module.azure.snowflake_sas_token_predictions
 
